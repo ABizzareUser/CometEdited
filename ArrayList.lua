d851bc3e417306d28a801e04ecf5e212bbd34bf7
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-10-12 19:56:10 +0300

    Update ArrayList.lua

--- ArrayList.lua
+++ ArrayList.lua
@@ -5,49 +5,49 @@ function rainbow()
     wait(1)
     local k = 1
     while k <= 255 do
-    Color3.new(k/255,0/255,0/255)
+    Text.TextColor3 = Color3.new(k/255,0/255,0/255)
     k = k + add
     wait()
     end
     while true do
     k = 1
     while k <= 255 do
-    Color3.new(255/255,k/255,0/255)
+    Text.TextColor3 = Color3.new(255/255,k/255,0/255)
     k = k + add
     wait()
     end
     k = 1
     while k <= 255 do
-    Color3.new(255/255 - k/255,255/255,0/255)
+    Text.TextColor3 = Color3.new(255/255 - k/255,255/255,0/255)
     k = k + add
     wait()
     end
     k = 1
     while k <= 255 do
-    Color3.new(0/255,255/255,k/255)
+    Text.TextColor3 = Color3.new(0/255,255/255,k/255)
     k = k + add
     wait()
     end
     k = 1
     while k <= 255 do
-    Color3.new(0/255,255/255 - k/255,255/255)
+    Text.TextColor3 = Color3.new(0/255,255/255 - k/255,255/255)
     k = k + add
     wait()
     end
     k = 1
     while k <= 255 do
-    Color3.new(k/255,0/255,255/255)
+    Text.TextColor3 = Color3.new(k/255,0/255,255/255)
     k = k + add
     wait()
     end
     k = 1
     while k <= 255 do
-    Color3.new(255/255,0/255,255/255 - k/255)
+    Text.TextColor3 = Color3.new(255/255,0/255,255/255 - k/255)
     k = k + add
     wait()
     end
     while k <= 255 do
-    Color3.new(255/255 - k/255,0/255,0/255)
+    Text.TextColor3 = Color3.new(255/255 - k/255,0/255,0/255)
     k = k + add
     wait()
     end
