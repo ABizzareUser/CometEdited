1dc3681d72147dfa60908d1731bf9f795b9a6ff2
ABizzareUser <115786359+ABizzareUser@users.noreply.github.com>
2022-10-14 18:07:51 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,26 +7,26 @@ spawn(function()
     local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
     game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
         if State == Enum.TeleportState.Started then
-            queueteleport('if betterisfile("CometEdited/Loader.lua") then loadstring(readfile("CometEdited/Loader.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Loader.lua"))()')
+            queueteleport('if betterisfile("CometV2/Loader.lua") then loadstring(readfile("CometV2/Loader.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Loader.lua"))()')
         end
     end)
 end)
 local placeid = game.PlaceId
 if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
-    if betterisfile("CometEdited/Modules/Bedwars.lua") then
-        loadstring(readfile("CometEdited/Modules/Bedwars.lua"))()
+    if betterisfile("CometV2/Modules/Bedwars.lua") then
+        loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/Bedwars.lua"))()
     end
 elseif placeid == 6872265039 then
-    if betterisfile("CometEdited/Modules/6872265039.lua") then
-        loadstring(readfile("CometEdited/Modules/6872265039.lua"))()
+    if betterisfile("CometV2/Modules/6872265039.lua") then
+        loadstring(readfile("CometV2/Modules/6872265039.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/6872265039.lua"))()
     end
 else
-    if betterisfile("CometEdited/Modules/CustomModulesTemplate.lua") then
-        loadstring(readfile("CometEdited/Modules/CustomModulesTemplate.lua"))()
+    if betterisfile("CometV2/Modules/CustomModulesTemplate.lua") then
+        loadstring(readfile("CometV2/Modules/CustomModulesTemplate.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/CustomModulesTemplate.lua"))()
     end
