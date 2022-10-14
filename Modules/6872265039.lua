e70ab636188ee8b5db30c4e5bfb7cc762e7240d7
ABizzareUser <115786359+ABizzareUser@users.noreply.github.com>
2022-10-14 17:43:01 +0800

    Update 6872265039.lua

--- Modules/6872265039.lua
+++ Modules/6872265039.lua
@@ -4,7 +4,7 @@ shared["CometConfigs"] = {
     Color = Color3.fromRGB(255,65,65),
     Enabled = false
 }
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/CustomCometV2/main/GuiLibrary.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzareUser/CometEdited/main/GuiLibrary.lua"))()
 local getasset = getsynasset or getcustomasset
 local ScreenGuitwo = game:GetService("CoreGui").RektskyNotificationGui
 local lplr = game:GetService("Players").LocalPlayer
