05135178a58df9af4e2a7f2906ee523c8828fb30
ABizzareUser <115786359+ABizzareUser@users.noreply.github.com>
2022-10-14 18:02:50 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -13,20 +13,20 @@ spawn(function()
 end)
 local placeid = game.PlaceId
 if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
-    if betterisfile("CometV2/Modules/Bedwars.lua") then
-        loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
+    if betterisfile("CometEdited/Modules/Bedwars.lua") then
+        loadstring(readfile("CometEdited/Modules/Bedwars.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/Bedwars.lua"))()
     end
 elseif placeid == 6872265039 then
-    if betterisfile("CometV2/Modules/6872265039.lua") then
-        loadstring(readfile("CometV2/Modules/6872265039.lua"))()
+    if betterisfile("CometEdited/Modules/6872265039.lua") then
+        loadstring(readfile("CometEdited/Modules/6872265039.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/6872265039.lua"))()
     end
 else
-    if betterisfile("CometV2/Modules/CustomModulesTemplate.lua") then
-        loadstring(readfile("CometV2/Modules/CustomModulesTemplate.lua"))()
+    if betterisfile("CometEdited/Modules/CustomModulesTemplate.lua") then
+        loadstring(readfile("CometEdited/Modules/CustomModulesTemplate.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ABizzarUser/CometEdited/main/Modules/CustomModulesTemplate.lua"))()
     end
